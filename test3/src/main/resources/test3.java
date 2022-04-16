059abe1516afe0892504d0e1b43c4ef5dc6461a8
smhrd <gnb1215@naver.com>
2022-04-16 14:47:38 +0900

    dd

---
+++ test3/src/main/resources/test3.java
@@ -0,0 +1,13 @@
+
+public class test3 {
+
+	public test3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 String id = "»Æ¿Œ"
+	}
+
+}
